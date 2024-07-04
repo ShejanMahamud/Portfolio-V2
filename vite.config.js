45c8307aa065a87a3774cb94074351dbcf47754c
Shejan Mahamud <mdshejanmahamud006@gmail.com>
2024-07-04 17:51:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory is set to 'dist'
-  },
 });
