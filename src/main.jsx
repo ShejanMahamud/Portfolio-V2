4fb34cd8e094fe624904185ac88f54c1964509f6
Shejan Mahamud <mdshejanmahamud006@gmail.com>
2024-09-28 17:28:49 +0000

    add

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Route from './Route/Route';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={Route} />
+    <SpeedInsights/>
   </React.StrictMode>
 );
